f863b84866c2539bf53e87453945783d19069976
Vladislav <belvladislav31@gmail.com>
2022-12-21 22:07:13 +0300

    pack was replaced by grid; louder sounds

--- main.py
+++ main.py
@@ -21,12 +21,18 @@ instr_text_string = '''(1) Как решить уравнение:
     • Деления на «/»;
     • Возведения в степень на «^».
 
-(3) Уравнение должно быть записано относительно строчной 
+(3) Уравнение должно быть записано относительно строчной
 переменной x (английская буква «x») с приравниванием к 0 или без.
 
 Примеры: 
+x ^ 3 + 1 = 0
 3 ^ 8 * x ^ 3 + 2 * x ^ 2 + 12 = 0
-(-x / 7.6 + 2) * (12 + x) * x ^ 3 - 21 ^ 2 / (3 * x - 4) ^ 2 - 12,1 * 3 ^ 21'''
+(-x / 7.6 + 2) * (12 + x) * x ^ 3 - 21 ^ 2 / (3 * x - 4) ^ 21 * 3 ^ 21
+
+*Любой текст можно копировать с помощью выделения, а затем
+нажатия сочетания клавиш ctrl + c. Копировать можно примеры,
+последние решения, найденные корни. Их можно листать, если
+все не вмещаются в поле вывода.'''
 
 check_error_find_roots_string = '''Возможно следует дописать уравнение, либо исключить
 русские буквы, английские (кроме «x»), спецсимволы (кроме «*», «/», «^» и скобок), пустую строку, ДЕЛЕНИЕ
@@ -48,12 +54,14 @@ def instr():
     win_instr.protocol('WM_DELETE_WINDOW',
                        lambda: (win_instr.destroy(), ws.PlaySound('sounds/pong.wav', NORMAL_SOUND_PLAY_BREAK)))
     # Для инструкции используется Text.
-    instr_text = tk.Text(win_instr, background='black', foreground='purple', font=('Calibri', 11),
-                         relief='solid', selectforeground='#00fee9', selectbackground='black')
-    instr_text.delete('1.0', '1111.1111')
-    instr_text.insert('1.0', instr_text_string)
-    instr_text.config(state='disabled')
-    instr_text.pack()
+    instr_scrolled_text = st.ScrolledText(win_instr, background='black', foreground='purple', font=('Calibri', 11),
+                                          relief='solid', selectforeground='#00fee9', selectbackground='black')
+    instr_scrolled_text.delete('0.0', 'end')
+    instr_scrolled_text.insert('0.0', instr_text_string)
+    instr_scrolled_text.tag_config('tag', font=('Calibri', 11, 'italic', 'bold'))
+    instr_scrolled_text.tag_add('tag', '18.0', 'end')
+    instr_scrolled_text.config(state='disabled')
+    instr_scrolled_text.pack()
     # Звук открытия, затем вызов автоматического закрытия через 200 с.
     ws.PlaySound('sounds/ping.wav', NORMAL_SOUND_PLAY_BREAK)
     win_instr.after(200000, lambda: win_instr.destroy())
@@ -73,13 +81,13 @@ def recent_solves():
     win_recent.protocol('WM_DELETE_WINDOW',
                         lambda: (win_recent.destroy(), ws.PlaySound('sounds/pong.wav', NORMAL_SOUND_PLAY_BREAK)))
     # Для недавних решений используется ScrolledText.
-    recent_solves_scrolled_text = st.ScrolledText(win_recent, background='black', foreground='purple',
-                                                  relief='solid', font=('Calibri', 11), selectforeground='#00fee9',
+    recent_solves_scrolled_text = st.ScrolledText(win_recent, background='black', foreground='purple', relief='solid',
+                                                  font=('Calibri', 11), selectforeground='#00fee9',
                                                   selectbackground='black')
-    recent_solves_scrolled_text.delete('1.0', '111.1111')
-    recent_solves_scrolled_text.insert('1.0', recent_solves_scrolled_text_string)
+    recent_solves_scrolled_text.delete('0.0', 'end')
+    recent_solves_scrolled_text.insert('0.0', recent_solves_scrolled_text_string)
     recent_solves_scrolled_text.tag_config('tag', justify='center')
-    recent_solves_scrolled_text.tag_add('tag', '1.0', '1111.1111')
+    recent_solves_scrolled_text.tag_add('tag', '0.0', 'end')
     recent_solves_scrolled_text.config(state='disabled')
     recent_solves_scrolled_text.pack()
     # Звук открытия, затем вызов автоматического закрытия через 200 с.
@@ -102,7 +110,7 @@ def check(_event):
         final_solve = solve(main_s)
     except (ZeroDivisionError, NameError, SyntaxError, TypeError):
         text_bottom.config(state='normal')
-        text_bottom.delete('1.0', '1111.1111')
+        text_bottom.delete('0.0', 'end')
         text_bottom.config(state='disabled')
         button_center.config(image=button_center_incorrect_photo)
         mb.showerror('Ошибка ввода', check_error_find_roots_string)
@@ -110,33 +118,22 @@ def check(_event):
     else:
         button_center.config(image=button_center_correct_photo)
         win_main.after(4000, lambda: button_center.config(image=button_center_default_photo))
-        if final_solve.count('x') > 1:
+        count_x = final_solve.count('x')
+        # Операции с Text.
+        text_bottom.config(state='normal')
+        text_bottom.delete('0.0', 'end')
+        if count_x > 0:
             ws.PlaySound('sounds/funky.wav', NORMAL_SOUND_PLAY_BREAK)
-            # Операции с Text.
-            text_bottom.config(state='normal')
-            text_bottom.delete('1.0', '1111.1111')
-            text_bottom.insert('1.0', f'Корни вашего уравнения:\n{final_solve}')
-            text_bottom.tag_config('tag', justify='center')
-            text_bottom.tag_add('tag', '1.0', '1111.1111')
-            text_bottom.config(state='disabled')
-        elif final_solve.count('x') == 1:
-            ws.PlaySound('sounds/funky.wav', NORMAL_SOUND_PLAY_BREAK)
-            # Операции с Text.
-            text_bottom.config(state='normal')
-            text_bottom.delete('1.0', '1111.1001')
-            text_bottom.insert('1.0', f'Корень вашего уравнения:\n{final_solve}')
-            text_bottom.tag_config('tag', justify='center')
-            text_bottom.tag_add('tag', '1.0', '1111.1001')
-            text_bottom.config(state='disabled')
+            if count_x > 1:
+                text_bottom.insert('0.0', f'Корни вашего уравнения:\n{final_solve}')
+            else:
+                text_bottom.insert('0.0', f'Корень вашего уравнения:\n{final_solve}')
         else:
             ws.PlaySound('sounds/boop.wav', NORMAL_SOUND_PLAY_BREAK)
-            # Операции с Text.
-            text_bottom.config(state='normal')
-            text_bottom.delete('1.0', '1111.1111')
             text_bottom.insert('1.0', 'Данное уравнение не имеет действительных корней')
-            text_bottom.tag_config('tag', justify='center')
-            text_bottom.tag_add('tag', '1.0', '1111.1111')
-            text_bottom.config(state='disabled')
+        text_bottom.tag_config('tag', justify='center')
+        text_bottom.tag_add('tag', '0.0', 'end')
+        text_bottom.config(state='disabled')
         # Добавление решений.
         recent_solves_scrolled_text_string += f"Решение уравнения:\n{main_s.replace('**', '^')}=0\n⤋\n{final_solve}\n"
     return 1
@@ -205,67 +202,72 @@ main_photo = tk.PhotoImage(file='resources/ico.png')
 win_main.iconphoto(True, main_photo)
 win_main.title('Equations')
 win_main.state('zoomed')
-win_main.minsize(950, 950)
+win_main.minsize(840, 700)
 win_main.config(bg='black')
 win_main.protocol('WM_DELETE_WINDOW', on_exit)
 
-# Все последующие элементы расположены в правильном порядке 'прилипания' друг к другу (на это указывает 'side='
-# (по умолчанию tk.TOP - константа), либо 'anchor=').
 # Кнопка помощи.
 
 button_left_photo = tk.PhotoImage(file='resources/lamp.png')
 button_left = tk.Button(win_main, image=button_left_photo, background='black', command=instr,
                         borderwidth=0, activebackground='black')
-button_left.pack(anchor=tk.NW, side=tk.LEFT, padx=15)
+button_left.grid(row=0, column=0, padx=10, sticky='nw')
 
 # Кнопка недавних.
 
 button_right_photo = tk.PhotoImage(file='resources/recent.png')
 button_right = tk.Button(win_main, image=button_right_photo, background='black', command=recent_solves,
                          borderwidth=0, activebackground='black')
-button_right.pack(anchor=tk.NE, side=tk.RIGHT, padx=15, pady=40)
-
-# Вспомогательный виджет, между кнопкой помощи и кнопкой недавних.
-label_top = tk.Label(background='black')
-label_top.pack(pady=35)
+button_right.grid(row=0, column=2, padx=10, pady=30, sticky='ne')
 
 # Поле ввода.
 
 entry_center = tk.Entry(win_main, font=('Segoe UI Variable Text Light', 50),
-                        bg='black', fg='#626262', width=25, justify=tk.CENTER,
-                        cursor='tcross',  # Курсоры: https://anzeljg.github.io/rin2/book2/2405/docs/tkinter/cursors.html
+                        bg='black', fg='#626262', width=20, justify=tk.CENTER,
+                        cursor='plus',  # Курсоры: https://anzeljg.github.io/rin2/book2/2405/docs/tkinter/cursors.html
                         insertbackground='#626262')
 entry_center.insert(0, 'Введите уравнение...')
-entry_center.pack(pady=30)
+entry_center.grid(row=1, column=1, sticky='we')
 
 # Кнопка решения уравнения.
 
-button_center_default_photo = tk.PhotoImage(file='resources/default_button.png')
-button_center_incorrect_photo = tk.PhotoImage(file='resources/incorrect_button.png')
-button_center_correct_photo = tk.PhotoImage(file='resources/correct_button.png')
+button_center_default_photo = tk.PhotoImage(file='resources/default.png')
+button_center_incorrect_photo = tk.PhotoImage(file='resources/incorrect.png')
+button_center_correct_photo = tk.PhotoImage(file='resources/correct.png')
 button_center = tk.Button(win_main, image=button_center_default_photo, background='black',
                           borderwidth=0, activebackground='black')
-button_center.pack(pady=30)
+button_center.grid(row=2, column=1)
 
 # Виджет (Label) неонового GIFa.
 
 label_center_photos = [tk.PhotoImage(file='resources/neon.gif', format=f'gif -index {i}') for i in range(52)]
 label_center = tk.Label(win_main, bg='black')
-label_center.pack(pady=40)
+label_center.grid(row=3, column=1)
 neon_gif_update(0)
 
 # Виджет (Text) корней.
 
-text_bottom = tk.Text(win_main, width=45, height=6, background='black', foreground='purple',
+text_bottom = tk.Text(win_main, width=45, height=4, background='black', foreground='purple',
                       font=('Segoe UI Variable Text Light', 20), relief='solid',
                       selectforeground='#00fee9', selectbackground='black')
-text_bottom.pack()
+text_bottom.grid(row=4, column=1)
 
 # Виджет (Label) кривой полосы снизу.
 
 label_bottom_photo = tk.PhotoImage(file='resources/curve_line.png')
 label_bottom = tk.Label(image=label_bottom_photo, background='black', activebackground='black')
-label_bottom.pack(side=tk.BOTTOM, pady=20)
+label_bottom.grid(row=5, column=1)
+
+# Позволяем всем использующимся колонкам и строкам увеличиваться, если есть доп. место.
+# По умолчанию weight=0 (или False), а weight=1 (или True).
+win_main.columnconfigure(0, weight=1)
+win_main.columnconfigure(1, weight=1)
+win_main.columnconfigure(2, weight=1)
+win_main.rowconfigure(1, weight=1)
+win_main.rowconfigure(2, weight=1)
+win_main.rowconfigure(3, weight=1)
+win_main.rowconfigure(4, weight=1)
+win_main.rowconfigure(5, weight=1)
 
 # Все события (клавиатура, мышь) и их описание: https://stackoverflow.com/questions/32289175/list-of-all-tkinter-events
 #                       |
